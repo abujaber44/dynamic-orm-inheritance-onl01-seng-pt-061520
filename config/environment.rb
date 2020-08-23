c7d825608f41027b479b0cc9d46290c3d2dd4c83
mjaber <abujaber44@gmail.com>
2020-08-23 11:06:00 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -13,3 +13,19 @@ SQL
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
+
+DB = {:conn => SQLite3::Database.new("db/dogs.db")}
+DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+
+sql_1 = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT
+  )
+SQL
+
+DB[:conn].execute(sql_1)
+DB[:conn].results_as_hash = true
+
+
